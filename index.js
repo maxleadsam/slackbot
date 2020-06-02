34f254e02b2e36ff79383aee70abc03e315fa554
Sam Vermeerssen <sam@MAXLEAD.LOCAL>
2020-06-02 22:58:46 +0200

    new walt slackbot

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const SlackBot = require('slackbots'); //bring in slackbot
 const axios = require('axios'); // bring in axios
 require('dotenv').config(); 
-console.log(process.env); // everything
 
 const bot = new SlackBot({
     token: process.env.API_KEY,
-    name: 'pete'
+    name: 'walt'
 });
 
 // Start Handler
@@ -14,6 +13,84 @@ bot.on('start', () => {
     const params = {
         icon_emjoi: ':smiley:'
     };
-    bot.postMessageToChannel('general', 'My name is Pete', params);
+    bot.postMessageToChannel('general', 'My name is Walt', params);
 });
 
+// Error Handler
+bot.on('error', err => console.log(err));
+
+// Message Handler - Callback will give us some data
+bot.on('message', data => {
+    if(data.type !== 'message') {
+        return;
+    }    
+        handleMessage(data.text);
+    });
+
+// response to Data
+function handleMessage(message) {
+    if(message.includes(' chucknorris')) {
+    chuckJoke();
+    } else if(message.includes(' yomama')) {
+    yoMamaJoke();
+        } else if(message.includes(' random')) {
+    randomJoke();
+         } else if(message.includes(' help')) {
+        getHelp();
+    }
+        }
+    
+
+// tell Chuck Norris joke. We use axios for the GET request, which gives us a promise
+function chuckJoke() {
+    axios.get('http://api.icndb.com/jokes/random')
+        .then(res => {
+            const joke = res.data.value.joke;
+            
+            const params = {
+                icon_emoji: ':laughing:'
+            };
+            bot.postMessageToChannel(
+                'general', 
+                `Chuck Norris: ${joke}`,
+                params
+                );
+        })
+}
+
+// tell yomama joke
+function yoMamaJoke() {
+axios.get('http://api.yomomma.info')
+.then(res => {
+    const joke = res.data.joke;
+    
+    const params = {
+        icon_emoji: ':laughing:'
+    };
+    bot.postMessageToChannel(
+        'general', 
+        `Yo Mama: ${joke}`,
+        params);
+});
+}
+
+// Tell a random Joke
+function randomJoke() {
+    const rand = Math.floor(Math.random() *2 ) +1;
+    if(rand === 1) {
+        chuckJoke();
+     } else if(rand === 2) {
+            yoMamaJoke();
+        }
+    }
+
+// help function
+function getHelp() {
+    const params = {
+        icon_emoji: ':question:'
+    };
+bot.postMessageToChannel('general', `Type @walt with either 'chucknorris, 'yomama' or 'random' to get a joke`,
+params);
+}
+
+// where to host a Slackbot?
