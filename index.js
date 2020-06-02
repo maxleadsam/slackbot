a04328b4189cb375702e64c9e1b3abc97cc28227
Sam Vermeerssen <sam@MAXLEAD.LOCAL>
2020-06-02 09:26:27 +0200

    slackbot code from tutorial

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// just a comment
+const SlackBot = require('slackbots'); //bring in slackbot
+const axios = require('axios'); // bring in axios
+require('dotenv').config(); 
+console.log(process.env); // everything
+
+const bot = new SlackBot({
+    token: process.env.API_KEY,
+    name: 'pete'
+});
+
+// Start Handler
+
+bot.on('start', () => {
+    const params = {
+        icon_emjoi: ':smiley:'
+    };
+    bot.postMessageToChannel('general', 'My name is Pete', params);
+});
+
